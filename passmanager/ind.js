49411f1a289fbf87f67872dce823e71265953ab7
Pulkit Chopra <95016298+Pulkit7902@users.noreply.github.com>
2023-11-26 14:26:36 +0530

    Add files via upload

---
+++ passmanager/ind.js
@@ -0,0 +1,71 @@
+const deletetable = (website) =>{
+    let data = localStorage.getItem("passwords")
+    let arr = JSON.parse(data)
+    arrupdate = arr.filter((e)=>{
+
+        return e.website !=website
+    })
+    localStorage.setItem("passwords" , JSON.stringify(arrupdate))
+    alert(`successfull deletion , ${website} , ${password.value}`)
+    showtable()
+
+}
+const showtable =() =>{
+let tb = document.querySelector("table")
+
+let data = localStorage.getItem("passwords");
+if(data == null){
+    tb.innerHTML = "No Data is there"
+}
+else{
+    let str =""
+    tb.innerHTML = `  <tr>
+    <th>Website</th>
+    <th>Usename</th>
+     <th>Password</th>
+     <th>Delete </th>
+   </tr>`
+    
+
+    let array = JSON.parse(data);
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    
+    str += `<tr>
+<td>${element.website}</td>
+<td>${element.username}</td>
+<td>${element.password}</td>
+<td> <button class = "btnse"  onclick="deletetable('${element.website}')">Delete</button></td>
+</tr>`
+website.value = "";
+username.value = ""
+password.username = ""
+
+}
+tb.innerHTML = tb.innerHTML + str;
+}}
+console.log("hello");
+showtable();
+
+document.querySelector(".btn").addEventListener("click" , (e)=>{
+    e.preventDefault();
+    console.log("clicked")
+    console.log(document.getElementById('username').value , document.getElementById('password').value)
+    let passwords = localStorage.getItem("passwords");
+
+
+if (passwords === null) {
+    let json = [];
+    json.push({ website:website.value,username: document.getElementById('username').value, password: document.getElementById('password').value });
+    alert("Pass saved");
+    localStorage.setItem("passwords", JSON.stringify(json));
+} else {
+    let json = JSON.parse(localStorage.getItem("passwords"));
+    json.push({website:website.value, username: document.getElementById('username').value, password: document.getElementById('password').value });
+    alert("Password saved");
+    localStorage.setItem("passwords", JSON.stringify(json));
+}
+console.log(passwords)
+showtable();
+}) 
